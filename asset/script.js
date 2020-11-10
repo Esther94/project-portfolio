b612de9f11b307c9251fd70d78ebf7583110b6c9
Davies Esther <daviesesther74@gmail.com>
2020-11-10 10:30:59 +0100

    creating folder and files

---
+++ asset/script.js
@@ -0,0 +1,26 @@
+// function signup(){
+    // confirm('Are you sure you want to submit the form?');
+// }
+
+function submitInfo() {
+    // this.signup();
+    var yourName = '';
+    var email = '';
+    var yourMessage = '';
+
+    yourName = document.getElementById('yourName').value;
+    console.log('your Name:' + yourName);
+    
+    email = document.getElementById('email').value;
+    console.log('email:' + email);
+
+    yourMessage = document.getElementById('yourMessageHere').value;
+    console.log('your Message:' + yourMessage);
+
+    if(yourName == '' || email == '' || yourMessage == ''){
+        alert('Input Missing Field');
+
+    } else {
+        alert('Form has been submitted');
+    }
+}
